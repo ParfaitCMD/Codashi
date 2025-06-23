48eb351523939789ee9b7d2a8afc9576f7b4c5bc
ParfaitCMD <devparfaitdev20@gmail.com>
2025-06-22 23:24:48 -0300

    Refazendo erro

--- Backend/src/main/java/com/codashi/Backend/BackendApplication.java
+++ Backend/src/main/java/com/codashi/Backend/BackendApplication.java
@@ -1,12 +1,14 @@
 package com.codashi.Backend; // Define o pacote onde esta classe está localizada. Pacotes ajudam a organizar as classes em grupos lógicos.
 
 import org.springframework.boot.SpringApplication; // Importa a classe SpringApplication, que é usada para iniciar a aplicação Spring Boot.
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // Anotação que marca esta classe como a principal da aplicação Spring Boot.
 // Ela combina três outras anotações:
 // @Configuration (indica que a classe pode conter configurações),
 // @EnableAutoConfiguration (ativa a configuração automática do Spring Boot),
 // @ComponentScan (ativa a varrimento de componentes, permitindo que o Spring encontre e registre beans automaticamente).
+@SpringBootApplication
 public class BackendApplication {
 
     // Método principal (main), ponto de entrada da aplicação Java.
